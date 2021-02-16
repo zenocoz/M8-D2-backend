3db4b3ad0f828972e68aff145de5f68568dc86d6
zenocoz <federico.soncini@gmail.com>
2021-02-16 19:38:14 +0100

    rewwrote in typsecript

--- server.js
+++ server.js
@@ -1,49 +1,30 @@
-const express = require("express")
-const cors = require("cors")
-const { join } = require("path")
-const listEndpoints = require("express-list-endpoints")
-const mongoose = require("mongoose")
-
-const usersRouter = require("./src/services/users")
-
-const {
-  notFoundHandler,
-  forbiddenHandler,
-  badRequestHandler,
-  genericErrorHandler,
-} = require("./src/services/errorHandlers")
-
-const server = express()
-
-server.use(cors())
-const port = process.env.PORT || 3005
-
+var express = require("express");
+var cors = require("cors");
+var join = require("path").join;
+var listEndpoints = require("express-list-endpoints");
+var mongoose = require("mongoose");
+var usersRouter = require("./src/services/users");
+var _a = require("./src/services/errorHandlers"), notFoundHandler = _a.notFoundHandler, forbiddenHandler = _a.forbiddenHandler, badRequestHandler = _a.badRequestHandler, genericErrorHandler = _a.genericErrorHandler;
+var server = express();
+server.use(cors());
+var port = process.env.PORT || 3005;
 // const staticFolderPath = join(__dirname, "../public")
 // server.use(express.static(staticFolderPath))
-server.use(express.json())
-
-server.use("/users", usersRouter)
-
+server.use(express.json());
+server.use("/users", usersRouter);
 // ERROR HANDLERS MIDDLEWARES
-
-server.use(badRequestHandler)
-server.use(forbiddenHandler)
-server.use(notFoundHandler)
-server.use(genericErrorHandler)
-
-console.log(listEndpoints(server))
-
+server.use(badRequestHandler);
+server.use(forbiddenHandler);
+server.use(notFoundHandler);
+server.use(genericErrorHandler);
+console.log(listEndpoints(server));
 // mongoose.set("debug", true)
-
 mongoose
-  .connect("mongodb://localhost:27017/TestAuth", {
+    .connect("mongodb://localhost:27017/TestAuth", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex: true,
-  })
-  .then(
-    server.listen(port, () => {
-      console.log("Server running on port", port)
-    })
-  )
-  .catch((err) => console.log(err))
+    useCreateIndex: true
+})
+    .then(server.listen(port, function () {
+    console.log("Server running on port", port);
+}))["catch"](function (err) { return console.log(err); });
